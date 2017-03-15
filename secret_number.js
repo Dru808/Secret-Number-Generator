16d26a99d96c65374d3206021d7f35bf31c7d68e
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-15 08:53:04 -1000

    finished secret

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+	var random = Math.floor((Math.random() * 1000001));
+
+		function getrepeatnumber(){
+			return random;
+		}
+	return getrepeatnumber;
 };
+
